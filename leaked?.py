edd37d981de29db0c15782b11f7bc048b2a577fe
Quack <79198231+iq7j@users.noreply.github.com>
2021-05-19 17:34:44 +0300

    Create leaked?.py

---
+++ leaked?.py
@@ -0,0 +1,15 @@
+from haveibeenpwnd import check_password
+
+print("""
+    __            _            _ ___
+   / /  ___  __ _| | _____  __| / _ \
+  / /  / _ \/ _` | |/ / _ \/ _` \// /
+ / /__|  __/ (_| |   <  __/ (_| | \/
+ \____/\___|\__,_|_|\_\___|\__,_| ()
+
+""")
+
+password = input("enter your password:")
+passwords =check_password(f'{password}')
+print("how many times have been you password in used in have i been pwned database:")
+print(passwords)
